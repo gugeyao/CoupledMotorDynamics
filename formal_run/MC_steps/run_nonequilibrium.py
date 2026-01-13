6d193fee70134e2ab124a2847141612e4ce8fb77
Geyao Gu <ggu7596@bismuth.theory.northwestern.edu>
2026-01-13 16:20:34 -0500

    added scripts for running JD under various MC step freq

---
+++ formal_run/MC_steps/run_nonequilibrium.py
@@ -0,0 +1,17 @@
+# Equilibrium check for the toy model
+import sys
+sys.path.append('/home/ggu7596/project/optimal_control/motorsim_double_track/toy_model_v2')
+from diffusion_jump_motor import DiffusionJumpMotor
+
+# motor object
+# keep the same parameters as the old simulation
+steps = 400000000
+MC_steps = 1
+eta = 0
+k_attach_far = 0.02
+coupling_strength = 0
+shifted_distance = 6
+core_size = 3
+barrier_height = 3.15
+motor = DiffusionJumpMotor(MC_steps = MC_steps, eta = eta, k_attach_far = k_attach_far, coupling_strength = coupling_strength, shifted_distance = shifted_distance,core_size = core_size, barrier_height=barrier_height)
+motor.parallel_run_simulation(parallel_jobs=1, steps=steps)
